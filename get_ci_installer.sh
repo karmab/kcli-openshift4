a618775d8c5aa6a3a954ec7f5abc9f0195ebca92
karmab <karimboumedhel@gmail.com>
2019-11-29 16:57:47 +0100

    use last octet of api_ip as virtual router id, to handle multiple installs on same network

--- get_ci_installer.sh
+++ get_ci_installer.sh
@@ -6,4 +6,5 @@ echo -e "${BLUE}Downloading latest openshift-install from registry.svc.ci.opensh
 export PULL_SECRET="openshift_pull.json"
 export VERSION=$(curl -s 'https://openshift-release.svc.ci.openshift.org/graph?format=dot' | grep tag | sed 's/.*label="\(.*.\)", shape=.*/\1/' | sort | tail -1)
 export OPENSHIFT_RELEASE_IMAGE=registry.svc.ci.openshift.org/ocp/release:$VERSION
+export OPENSHIFT_RELEASE_IMAGE=registry.svc.ci.openshift.org/ocp/release:4.4
 oc adm release extract --registry-config $PULL_SECRET --command=openshift-install --to . $OPENSHIFT_RELEASE_IMAGE
